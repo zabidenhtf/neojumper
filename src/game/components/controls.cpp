f47e6eca616ad5eb117990fbe96d13dd623d65a7
Полищук Никита <policyaskrimerov@gmail.com>
2026-01-01 13:34:36 +0300

    Fixed controls on big screens

--- src/game/components/controls.cpp
+++ src/game/components/controls.cpp
@@ -46,46 +46,49 @@ void controls::update(double tick){
 }
 
 void controls::render(){
-    int width = 300*gfx::screen_aspect();
+    float width = screen_width / 1.5;
+    float height = screen_height / 1.5;
     gfx::set_viewport(0,0,screen_width, screen_height);
-    gfx::set_ortho(0,0, width,300);
+    gfx::set_ortho(0,0, width,height);
 
     // pretty line
     for (int i = 0; i<screen_width/64; i++){
         gfx::enable_texture(data2d::textures[BUTTONS_LINE]);
-        gfx::draw_2d_quad(vec2(i*64, 300-size/2-4), vec2(64, 8),vec4(1,1,1,1));
+        gfx::draw_2d_quad(vec2(i*64, height-size/2-4), vec2(64, 8),vec4(1,1,1,1));
         gfx::disable_texture();
     }
 
     // Making many buttons of dance, and then animate it
     for (int i = 0; i < width/(distance+size)+1; i++){ // Optimisated
-        switch (game->now_dance.movements[i+step]){
-        case JUMP:
-            gfx::enable_texture(data2d::textures[BUTTON_JUMP]);
-            break;
-        case FLIP:
-            gfx::enable_texture(data2d::textures[BUTTON_FLIP]);
-            break;
-        case KICK_LEFT:
-            gfx::enable_texture(data2d::textures[BUTTON_KICK_LEFT]);
-            break;
-        case KICK_RIGHT:
-            gfx::enable_texture(data2d::textures[BUTTON_KICK_RIGHT]);
-            break;
-        case SHUFFLE_FORWARD:
-            gfx::enable_texture(data2d::textures[BUTTON_SHUFFLE_FORWARD]);
-            break;
-        case SHUFFLE_BACKWARD:
-            gfx::enable_texture(data2d::textures[BUTTON_SHUFFLE_BACKWARD]);
-            break;
+        if (i + step < game->now_dance.movements.size()){
+            switch (game->now_dance.movements[i+step]){
+                case JUMP:
+                    gfx::enable_texture(data2d::textures[BUTTON_JUMP]);
+                    break;
+                case FLIP:
+                    gfx::enable_texture(data2d::textures[BUTTON_FLIP]);
+                    break;
+                case KICK_LEFT:
+                    gfx::enable_texture(data2d::textures[BUTTON_KICK_LEFT]);
+                    break;
+                case KICK_RIGHT:
+                    gfx::enable_texture(data2d::textures[BUTTON_KICK_RIGHT]);
+                    break;
+                case SHUFFLE_FORWARD:
+                    gfx::enable_texture(data2d::textures[BUTTON_SHUFFLE_FORWARD]);
+                    break;
+                case SHUFFLE_BACKWARD:
+                    gfx::enable_texture(data2d::textures[BUTTON_SHUFFLE_BACKWARD]);
+                    break;
+            }
+            float x = i*(size+distance);
+            float time_x = step_time*(size+distance)/speed;
+            gfx::draw_2d_quad(vec2(x - time_x + size/2, height-size), vec2(size, size),vec4(1,1,1,1));
+            gfx::disable_texture();
         }
-        float x = i*(size+distance);
-        float time_x = step_time*(size+distance)/speed;
-        gfx::draw_2d_quad(vec2(x - time_x + size/2, 300-size), vec2(size, size),vec4(1,1,1,1));
-        gfx::disable_texture();
     }
     // helpfull arrow
     gfx::enable_texture(data2d::textures[BUTTON_ARROW]);
-    gfx::draw_2d_quad(vec2(size/8, 300-size), vec2(size/4, size/4),vec4(1,1,1,1));
+    gfx::draw_2d_quad(vec2(size/8, height-size), vec2(size/4, size/4),vec4(1,1,1,1));
     gfx::disable_texture();
 }
